f1e75223d192c99ba5bae3e54d5b0d90ea3fa353
serhiilikh <serhii.likhouzov@indap.tech>
2020-05-20 15:38:16 +0300

    removed redis&celery, upd requirements

---
+++ fire.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import os
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+import django
+import time
+
+
+def upvotes_reset():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'test_task.settings')
+    django.setup()
+    from core.models import Upvote
+    while True:
+        time.sleep(60 * 60 * 24)
+        Upvote.objects.all().delete()
+
+
+
+Thread(target=upvotes_reset, daemon=True).start()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'test_task.settings')
+application = get_wsgi_application()
+call_command('runserver', '127.0.0.1:8000')
